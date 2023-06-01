854eaf2917af006200a82427cf24aa6454900151
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2023-06-01 15:18:48 +0700

    apply design

--- src/1th/first-app/src/ItemDisplay.tsx
+++ src/1th/first-app/src/ItemDisplay.tsx
@@ -15,14 +15,13 @@ function ItemDisplay({ item }: ItemDisplayProps) {
   return (
     <div>
       {item && (
-        <div>
-          <img src={item.imageUrl} alt={item.name} />
-          {item.imageUrl && <p>{item.name}</p>}
+        <div className="mt-4">
+          <img className="w-32 h-32 rounded-full mx-auto mb-2" src={item.imageUrl} alt={item.name} />
+          <p className="text-sm text-gray-500 text-center mt-2">{item.name}</p>
         </div>
       )}
     </div>
   );
 }
-
 
 export default ItemDisplay;
