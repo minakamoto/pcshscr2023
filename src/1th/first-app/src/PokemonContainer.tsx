e07c4d6e8001861e773aff521a6936ef4a9a4f7c
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2023-06-01 14:52:01 +0700

    separate PokemonContainer component from App.tsx

---
+++ src/1th/first-app/src/PokemonContainer.tsx
@@ -0,0 +1,44 @@
+// PokemonContainer.tsx
+
+// Move all the parts of App.tsx related to the PokemonContainer component to this file.
+import React, { useState } from "react";
+import axios from "axios";
+
+function PokemonContainer() {
+  interface Pokemon {
+    name: string;
+    imageUrl: string;
+  }
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  
+  const handleClick = async () => {
+    try {
+      const randomId = Math.floor(Math.random() * 1000) + 1;
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${randomId}`
+      );
+      setPokemon({
+        name: response.data.name,
+        imageUrl: response.data.sprites.front_default,
+      });
+    } catch (error) {
+      console.error("Error fetching Pokemon:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Random Pokemon Image Generator</h1>
+      <button onClick={handleClick}>Generate Image</button>
+      {pokemon && (
+        <div>
+          <img src={pokemon.imageUrl} alt={pokemon.name} />
+          {pokemon.imageUrl && <p>{pokemon.name}</p>}
+        </div>
+      )}
+    </div>
+  );
+}
+
+// It can be called from other components.
+export default PokemonContainer;
