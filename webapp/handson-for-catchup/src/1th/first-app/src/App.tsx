b617a91da7166f5fd0c6f0f35f03d511e9f4a3db
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2023-06-02 16:04:54 +0700

    Change to vite
    Separate PokemonContainer component from App.tsx

--- webapp/handson-for-catchup/src/1th/first-app/src/App.tsx
+++ webapp/handson-for-catchup/src/1th/first-app/src/App.tsx
@@ -1,48 +1,9 @@
 // App.tsx
 
-import { useState } from "react";
-import axios from "axios";
+// Removed import statements that are no longer needed
+// PokemonContainer component is imported for use in App.tsx
+import PokemonContainer from "./PokemonContainer";
 
-// Define PokemonContainer component
-// Everything that is related to the processing of the random image display in App.tsx is defined in this component.
-function PokemonContainer() {
-  interface Pokemon {
-    name: string;
-    imageUrl: string;
-  }
-
-  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-  
-  const handleClick = async () => {
-    try {
-      const randomId = Math.floor(Math.random() * 1000) + 1;
-      const response = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${randomId}`
-      );
-      setPokemon({
-        name: response.data.name,
-        imageUrl: response.data.sprites.front_default,
-      });
-    } catch (error) {
-      console.error("Error fetching Pokemon:", error);
-    }
-  };
-
-  return (
-    <div>
-      <h1>Random Pokemon Image Generator</h1>
-      <button onClick={handleClick}>Generate Image</button>
-      {pokemon && (
-        <div>
-          <img src={pokemon.imageUrl} alt={pokemon.name} />
-          {pokemon.imageUrl && <p>{pokemon.name}</p>}
-        </div>
-      )}
-    </div>
-  );
-}
-
-// use the PokemonContainer component
 function App() {
   return (
     <div>
