c06aba39de96c7b189d0b9cfe5c26f9826fd22ba
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2023-06-21 10:55:30 +0700

    extract specific language config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,66 +1,7 @@
-import { DefaultTheme, LocaleSpecificConfig, defineConfigWithTheme } from 'vitepress'
-
-
-const enConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Web App', link: '/README' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Hands-on for catch-up',
-        items: [
-          { text: 'Introduction', link: '/README' },
-          { text: 'Overview of web app technology stack', link: '/0th' },
-          { text: 'Hands-on to develop simple & easy UI', link: '/1st' },
-        ]
-      }
-    ],
-  }
-}
-
-const thConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
-  themeConfig: {
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Web App', link: '/th/README' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Hands-on for catch-up',
-        items: [
-          { text: 'บทนำ', link: '/th/README' },
-          { text: 'ภาพรวมเกี่ยวกับเทคโนโลยีสำหรับพัฒนาเว็บแอปพลิเคชัน', link: '/th/0th' },
-          { text: 'ภาคปฏิบัติ(hands-on)เพื่อพัฒนา UI ที่ง่ายและสะดวก', link: '/th/1st' },
-        ]
-      }
-    ],
-  }
-}
-
-const jaConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
-  themeConfig: {
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Web App', link: '/ja/README' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Hands-on for catch-up',
-        items: [
-          { text: 'はじめに', link: '/ja/README' },
-          { text: 'Webアプリに関わる技術スタックの概要', link: '/ja/0th' },
-          { text: 'シンプルかつ簡単なUIを開発するハンズオン', link: '/ja/1st' },
-        ]
-      }
-    ],
-  }
-}
+import { defineConfigWithTheme } from 'vitepress'
+import { enConfig } from './enConfig'
+import { thConfig } from './thConfig'
+import { jaConfig } from './jaConfig'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfigWithTheme({
---
+++ docs/.vitepress/enConfig.ts
@@ -0,0 +1,22 @@
+import { DefaultTheme, LocaleSpecificConfig } from 'vitepress';
+
+export const enConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Web App', link: '/README' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Hands-on for catch-up',
+        items: [
+          { text: 'Introduction', link: '/README' },
+          { text: 'Overview of web app technology stack', link: '/0th' },
+          { text: 'Hands-on to develop simple & easy UI', link: '/1st' },
+        ]
+      }
+    ],
+  }
+};
