ffa6b5e2e706dd3ef5645b0dc3d77d629186dfc9
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2023-08-29 16:38:47 +0700

    add 2nd hands-on to config and README

--- docs/.vitepress/thConfig.ts
+++ docs/.vitepress/thConfig.ts
@@ -1,21 +1,31 @@
-import { DefaultTheme, LocaleSpecificConfig } from 'vitepress';
+import { DefaultTheme, LocaleSpecificConfig } from "vitepress";
 
 export const thConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Web App', link: '/th/README' }
+      { text: "Home", link: "/" },
+      { text: "Web App", link: "/th/README" },
     ],
 
     sidebar: [
       {
-        text: 'Hands-on for catch-up',
+        text: "Hands-on for catch-up",
         items: [
-          { text: 'บทนำ', link: '/th/README' },
-          { text: 'ภาพรวมเกี่ยวกับเทคโนโลยีสำหรับพัฒนาเว็บแอปพลิเคชัน', link: '/th/0th' },
-          { text: 'ภาคปฏิบัติ(hands-on)เพื่อพัฒนา UI ที่ง่ายและสะดวก', link: '/th/1st' },
-        ]
-      }
+          { text: "บทนำ", link: "/th/README" },
+          {
+            text: "ภาพรวมเกี่ยวกับเทคโนโลยีสำหรับพัฒนาเว็บแอปพลิเคชัน",
+            link: "/th/0th",
+          },
+          {
+            text: "ภาคปฏิบัติ(hands-on)เพื่อพัฒนา UI ที่ง่ายและสะดวก",
+            link: "/th/1st",
+          },
+          {
+            text: "ภาคปฏิบัติ(hands-on)เพื่อพัฒนา UI ที่ซับซ้อนขึ้นเล็กน้อยและ API ของตัวเอง",
+            link: "/th/2nd",
+          },
+        ],
+      },
     ],
-  }
+  },
 };
